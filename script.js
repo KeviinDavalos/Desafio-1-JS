6df5f8b76e9eb9f559cbec3520a66db90a1d7eb3
Kevin Davalos <kevin.davalos.01@gmail.com>
2022-07-27 03:21:20 -0300

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //Pedirle al usuario un numero para luego escribir la tabla de multiplicar
-let num = parseInt(prompt("Ingrese un numero para detallar la tabla"));
-for (i = 1; i<=10; i++){
-    multiresul = num *i;
-    console.log(num + "X"+ i + "=" + multiresul);
+// let num = parseInt(prompt("Ingrese un numero para detallar la tabla"));
+// for (i = 1; i<=10; i++){
+//     multiresul = num *i;
+//     console.log(num + "X"+ i + "=" + multiresul);
+// }
+
+//Pedir un numero por prompt, repetir la salida del mensaje "Hola" la cantidad de veces ingresada.
+let num = parseInt(prompt("Ingrese un numero para escribir la cantidad de veces Hola"))
+let palabra = "Hola"
+for (let i = 1; i<=num; i++){
+    console.log(i + " " + palabra);
 }
